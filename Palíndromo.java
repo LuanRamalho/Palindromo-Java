32d96a58270a1dbca75490ab2914ca297ddd899d
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-01-03 20:27:15 -0300

    Add files via upload

---
+++ Palíndromo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class Palíndromo 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+    		
+    		System.out.println("Digite uma palavra ou frase:");
+    		String palavra = sc.nextLine();
+    		String palindromo = "";
+    		
+    		for(int i = palavra.length() -1; i >= 0; i--){
+    			
+    			palindromo += palavra.charAt(i);
+
+    		}
+                
+                System.out.println("");
+                
+    		
+    		if(palindromo.equals(palavra)) {
+    			System.out.print("É um palíndromo!" + "\n" + palindromo);
+                        System.out.println("");
+                        System.out.println("");
+    		}
+    		else {
+    			System.out.print("Não é um palíndromo!" + "\n" + palindromo);
+                        System.out.println("");
+                        System.out.println("");
+    			
+    		}
+    		sc.close();
+    	}
+
+    }
+    
+
